38c2547c84aecd907eebc524372e593aca50aeae
ReeceCole <reececharlescole@gmail.com>
2022-09-07 14:29:34 -0500

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -1,9 +1,10 @@
+#Parent Class
 class User:
     name = 'No Name Provided'
     email = ' '
     password = '1234abcd'
     account_number = 0
-
+#Child Class
 class Employee(User):
     base_pay = 11.00
     department = 'General'
