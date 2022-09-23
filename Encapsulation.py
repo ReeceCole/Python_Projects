71594bc1708ade871e4a2bae7f121113fa784165
ReeceCole <reececharlescole@gmail.com>
2022-09-23 15:27:35 -0500

    Updated Encapsulation Assignment

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,6 +1,7 @@
 class Protected:
     def __init__(self):
-        self.__privateVar = 12 
+        self.__privateVar = 12
+        self._protectedVar = 0
 
     def getPrivate(self):
         print(self.__privateVar)
@@ -12,3 +13,8 @@ obj = Protected()
 obj.getPrivate()
 obj.setPrivate(23)
 obj.getPrivate()
+obj = Protected()
+obj._protectedVar = 34
+print(obj._protectedVar)
+
+
