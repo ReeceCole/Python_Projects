e416b74d26570673065a06a86f96c15c7bf2b5ca
ReeceCole <reececharlescole@gmail.com>
2022-10-03 16:18:16 -0500

    Updated web page generator assignment

--- web_page_generator.py
+++ web_page_generator.py
@@ -6,21 +6,27 @@ class ParentWindow(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.master.title("Web Page Generator")
-        self.btn = Button(self.master, text="Default HTML Page", width=30, height=2, command=self.defaultHTML,)
-        self.btn.grid(padx=(10,10) , pady=(10,10))
+        self.btn = Button(self.master, text="Default HTML Page", width=30, height=2, command=self.defaultHTML)
+        self.btn2 = Button(self.master, text="Submit Custom Text", width=30, height=2, command=self.customText)
+        self.btn2.grid(row=2, padx=(10,10) , pady=(10,10), column=0)
+        self.btn.grid(row=2, padx=(10,10) , pady=(10,10), column=1)
+        self.entry = Entry(self.master, width=42)
+        self.entry.grid(row=1, padx=(10,10) , pady=(10,10), columnspan=2)
+        self.label = Label(self.master, text="")
+        self.label.grid()
+        self.lblDisplay = Label(self.master, text= "Enter custom text or click the Default HTML page button").grid(row=0, padx=(10,10) , pady=(10,10), columnspan=2)
         
-
-        def get_data():
-            label.config(text= entry.get())
-            entry = Entry(self, width=42)
-            entry.place(relx= .5, rely= .5, anchor= CENTER)
-            label = Label(win, text="")
-            label.pack()
-            self.lblDisplay(Frame, text= "{}", command= get_data).place(relx= .7, rely= .5, anchor= CENTER)
     
-
     def defaultHTML(self):
         htmlText = "Stay tuned for amazing summer sale!"
+        htmlFile = open("index.html", "w")
+        htmlContent = "<html>\n<body>\n<h1>" + htmlText + "</h1>\n</body>\n</html>"
+        htmlFile.write(htmlContent)
+        htmlFile.close()
+        webbrowser.open_new_tab("index.html")
+
+    def customText(self):
+        htmlText = self.entry.get()
         htmlFile = open("index.html", "w")
         htmlContent = "<html>\n<body>\n<h1>" + htmlText + "</h1>\n</body>\n</html>"
         htmlFile.write(htmlContent)
